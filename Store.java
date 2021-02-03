445fedeab3561d62e611277d080dbd31d650dd69
Tiago Rossi <34634082+tiagorossig@users.noreply.github.com>
2021-02-03 14:04:09 -0600

    added required runtimes

--- Store.java
+++ Store.java
@@ -1,17 +1,21 @@
 import java.util.*;
 
 public class Store {
+    // Required runtime: O(1)
     public Store() { 
     }
 
+    // Required runtime: O(log(n))
     public String put(String key, int value) {
         return "TPEO";
     }
 
+    // Required runtime: O(log(n))
     public String get(String key) {
         return "TPEO";
     }
 
+    // Required runtime: O(log(n))
     public String get(String key, int version) {
         return "TPEO";
     }
